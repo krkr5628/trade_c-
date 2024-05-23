b4c30c0ecaede678bf2de19754cec719b50da9fc
Seong Yeop Choi <krkr5628@gmail.com>
2024-05-23 09:18:52 +0900

    index 체크에서 바뀌어 있었

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -741,7 +741,7 @@ namespace WindowsFormsApp1
             this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.90253F));
             this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.43321F));
             this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.53069F));
-            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 217F));
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 220F));
             this.tableLayoutPanel5.Controls.Add(this.setting_allowed, 4, 0);
             this.tableLayoutPanel5.Controls.Add(this.setting_open, 3, 0);
             this.tableLayoutPanel5.Controls.Add(this.save_button, 2, 0);
@@ -762,7 +762,7 @@ namespace WindowsFormsApp1
             this.setting_allowed.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.setting_allowed.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.setting_allowed.ForeColor = System.Drawing.Color.SpringGreen;
-            this.setting_allowed.Location = new System.Drawing.Point(665, 2);
+            this.setting_allowed.Location = new System.Drawing.Point(663, 2);
             this.setting_allowed.Margin = new System.Windows.Forms.Padding(2);
             this.setting_allowed.Name = "setting_allowed";
             this.setting_allowed.Size = new System.Drawing.Size(103, 29);
@@ -775,7 +775,7 @@ namespace WindowsFormsApp1
             this.setting_open.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.setting_open.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.setting_open.ForeColor = System.Drawing.Color.SpringGreen;
-            this.setting_open.Location = new System.Drawing.Point(569, 2);
+            this.setting_open.Location = new System.Drawing.Point(567, 2);
             this.setting_open.Margin = new System.Windows.Forms.Padding(2);
             this.setting_open.Name = "setting_open";
             this.setting_open.Size = new System.Drawing.Size(92, 29);
@@ -788,7 +788,7 @@ namespace WindowsFormsApp1
             this.save_button.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.save_button.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.save_button.ForeColor = System.Drawing.Color.SpringGreen;
-            this.save_button.Location = new System.Drawing.Point(467, 2);
+            this.save_button.Location = new System.Drawing.Point(465, 2);
             this.save_button.Margin = new System.Windows.Forms.Padding(2);
             this.save_button.Name = "save_button";
             this.save_button.Size = new System.Drawing.Size(98, 29);
@@ -801,11 +801,11 @@ namespace WindowsFormsApp1
             this.setting_name.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.setting_name.Location = new System.Drawing.Point(129, 2);
+            this.setting_name.Location = new System.Drawing.Point(128, 2);
             this.setting_name.Margin = new System.Windows.Forms.Padding(2);
             this.setting_name.Name = "setting_name";
             this.setting_name.ReadOnly = true;
-            this.setting_name.Size = new System.Drawing.Size(334, 21);
+            this.setting_name.Size = new System.Drawing.Size(333, 21);
             this.setting_name.TabIndex = 1;
             // 
             // label5
@@ -818,7 +818,7 @@ namespace WindowsFormsApp1
             this.label5.Location = new System.Drawing.Point(2, 0);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(123, 33);
+            this.label5.Size = new System.Drawing.Size(122, 33);
             this.label5.TabIndex = 9;
             this.label5.Text = "전략명";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -2715,13 +2715,13 @@ namespace WindowsFormsApp1
             this.tableLayoutPanel12.Controls.Add(this.label21, 0, 3);
             this.tableLayoutPanel12.Controls.Add(this.type1_end, 4, 0);
             this.tableLayoutPanel12.Controls.Add(this.type1_start, 2, 0);
-            this.tableLayoutPanel12.Controls.Add(this.type3_start, 2, 1);
-            this.tableLayoutPanel12.Controls.Add(this.type2_start, 2, 2);
             this.tableLayoutPanel12.Controls.Add(this.type4_start, 2, 3);
             this.tableLayoutPanel12.Controls.Add(this.type5_start, 2, 4);
             this.tableLayoutPanel12.Controls.Add(this.label26, 3, 4);
             this.tableLayoutPanel12.Controls.Add(this.type5_end, 4, 4);
             this.tableLayoutPanel12.Controls.Add(this.type1_selection, 1, 0);
+            this.tableLayoutPanel12.Controls.Add(this.type3_start, 2, 2);
+            this.tableLayoutPanel12.Controls.Add(this.type2_start, 2, 1);
             this.tableLayoutPanel12.Location = new System.Drawing.Point(2, 50);
             this.tableLayoutPanel12.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel12.Name = "tableLayoutPanel12";
@@ -2969,7 +2969,7 @@ namespace WindowsFormsApp1
             this.type3_start.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.type3_start.Location = new System.Drawing.Point(102, 21);
+            this.type3_start.Location = new System.Drawing.Point(102, 40);
             this.type3_start.Margin = new System.Windows.Forms.Padding(2);
             this.type3_start.Name = "type3_start";
             this.type3_start.Size = new System.Drawing.Size(58, 20);
@@ -2980,7 +2980,7 @@ namespace WindowsFormsApp1
             this.type2_start.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.type2_start.Location = new System.Drawing.Point(102, 40);
+            this.type2_start.Location = new System.Drawing.Point(102, 21);
             this.type2_start.Margin = new System.Windows.Forms.Padding(2);
             this.type2_start.Name = "type2_start";
             this.type2_start.Size = new System.Drawing.Size(58, 20);
